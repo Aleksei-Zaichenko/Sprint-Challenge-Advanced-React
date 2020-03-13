160728e9991d3f78553788ad628b7b04650656fe
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-03-13 11:45:04 -0400

    completed steps 1 and 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      players:[]
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        // res.data.message
+        //console.log(res.data);
+        this.setState({
+          players: res.data
+        });
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  render(){
+    console.log(this.state.players)
+    return (
+      <div className = "App">
+        <h1>Hello Players</h1>
+        {this.state.players.map(player => (
+            <div className="player" key={player.id} >
+              <h3>Name: {player.name}</h3>
+              <h3>Country: {player.country}</h3>
+              <h3>Searches: {player.searches}</h3>
+              <h3>Id: {player.id}</h3>
+            </div>
+          ))}
+      </div>
+    )
+  }
 }
 
 export default App;
